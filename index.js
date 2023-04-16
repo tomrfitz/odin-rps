9463e301c65b6fdedad639c9952ebe6f6b396c33
Thomas FitzGerald <tomrfitz@gmail.com>
2023-04-16 16:46:58 -0400

    creating scoretable to record result history

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ function playRound(playerSelection) {
     var userChoice = playerSelection.toLowerCase();
   }
   var computerChoice = getComputerChoice();
+  var playerPlay = document.getElementById("playerChoice");
+  var computerPlay = document.getElementById("computerChoice");
+
+  playerPlay.textContent = userChoice;
+  computerPlay.textContent = computerChoice;
   console.log("User: " + userChoice);
   console.log("Computer: " + computerChoice);
   if (userChoice === computerChoice) {
@@ -38,30 +43,51 @@ function playRound(playerSelection) {
   }
 }
 
-function game() {
-  let userWin = 0;
-  let computerWin = 0;
-  let tie = 0;
-  for (let i = 0; i++; ) {
-    result = playRound();
-    console.log(result);
-    if (result === "User wins!") {
-      userWin++;
-    } else if (result === "Computer wins!") {
-      computerWin++;
-    } else {
-      tie++;
-    }
+function game(id) {
+  result = playRound(id);
+  console.log(result);
+  var scoretable = document.getElementById("scoretable");
+  var row = scoretable.insertRow(gameNumber);
+  var gameCell = row.insertCell(0);
+  gameCell.innerHTML = gameNumber;
+  var winnerCell = row.insertCell(1);
+  var userCell = row.insertCell(2);
+  var computerCell = row.insertCell(3);
+  var tieCell = row.insertCell(4);
+  var recentWinner = document.getElementById("recentWinner");
+  if (result === "User wins!") {
+    userWin++;
+    winnerCell.innerHTML = "User";
+    recentWinner.innerHTML = "User";
+  } else if (result === "Computer wins!") {
+    computerWin++;
+    winnerCell.innerHTML = "Computer";
+    recentWinner.innerHTML = "Computer";
+  } else {
+    tie++;
+    winnerCell.innerHTML = "Tie";
+    recentWinner.innerHTML = "Tie";
   }
+  userCell.innerHTML = userWin;
+  computerCell.innerHTML = computerWin;
+  tieCell.innerHTML = tie;
+
+  gameNumber++;
+  console.log("Game number: " + gameNumber);
   console.log("User wins: " + userWin);
   console.log("Computer wins: " + computerWin);
   console.log("Ties: " + tie);
 }
 
+let userWin = 0;
+let computerWin = 0;
+let tie = 0;
+let gameNumber = 1;
+
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
   button.addEventListener("click", function () {
-    playRound(button.id);
+    game(button.id);
   });
 });
